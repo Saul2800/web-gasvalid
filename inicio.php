3cd4520c0e2030b34cf9065afde5cd6ac16f5581
Saul Arenas <Saul@MacBooks-MBP.lan>
2020-12-21 14:20:11 -0600

    Se aniade propuesta de inicio con su respectivo css

---
+++ inicio.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>GasValid inicio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/inicio.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<nav id="nav">
+						<a href="login.php">Login</a>
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Bienvenido a GasValid</h1>
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<h3>TITULO1</h3>
+							<p>parrafo1</p>
+						</div>
+
+						<div>
+							<h3>TITULO2</h3>
+							<p>parrafo2</p>
+						</div>
+						<div>
+							<h3>TITULO3</h3>
+							<p>parrafo3</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<div class="image round">
+								<img src="recursos/imagen1.jpg" alt="Pic 01" />
+							</div>
+							<header>
+								<h3>TITULO4</h3>
+							</header>
+							<p>parrafo4</p>
+						</article>
+						<article>
+							<div class="image round">
+								<img src="recursos/imagen2.jpg" alt="Pic 02" />
+							</div>
+							<header>
+								<h3>TITULO5</h3>
+							</header>
+							<p>parrafo5</p>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+
+					<h3>En contacto</h3>
+
+					<form action="#" method="post">
+
+						<div class="field half first">
+							<label for="name">Nombre</label>
+							<input name="name" id="name" type="text" placeholder="Name">
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input name="email" id="email" type="email" placeholder="Email">
+						</div>
+						<div class="field">
+							<label for="message">Mensaje</label>
+							<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input value="Send Message" class="button alt" type="submit"></li>
+						</ul>
+					</form>
+
+					<div class="copyright">
+						<a href="https://www.kaabcode.com/">Kaab_Code</a>
+					</div>
+
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>GV Login</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/main.css" />
+	</head>
+	<body>
+		aqui se trabaja el login
+	</body>
+</html>
