2145a64a3a5a66dbc2504aa051a2a030f6da4fd7
RobertoNeri <75956181+RobertoNeri@users.noreply.github.com>
2020-12-26 14:32:13 -0600

    Add files via upload
    modifique solo la conexion con los nombres de las imagenes que se muestran en pestaña y el icono que se muestra junto al login y la imagen de fondo.

---
+++ css/inicio.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>GasValid inicio</title>
+		<meta charset="utf-8" />
+		<link rel ="icon" type="icon/png" href="recursos/gasvalid_logo.png" >      <!--Icono a pestaña-->
+		<meta name="viewport" content="width=device-width, initial-scale=1" />	<!--Para cels -->
+		<link rel="stylesheet" href="css/inicio.css" />							<!--css inicio -->	
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<nav id="nav">
+						<a href="login.php">Login</a>
+						<img src="recursos/gasvalid_logo.png" width="100">							<!--Dirige al login-->
+					</nav>
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Bienvenido a GasValid</h1>							<!--TITULO -->
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<h3>TITULO1</h3>								<!--info-->
+							<p>parrafo1</p>									<!--info-->
+						</div>
+
+						<div>
+							<h3>TITULO2</h3>								<!--info-->
+							<p>parrafo2</p>									<!--info-->
+						</div>
+						<div>
+							<h3>TITULO3</h3>								<!--info-->
+							<p>parrafo3</p>									<!--info-->
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<div class="image round">
+								<img src="recursos/imagen1.jpg" alt="Pic 01" />		<!--imagen-->
+							</div>
+							<header>
+								<h3>TITULO4</h3>									<!--info-->
+							</header>
+							<p>parrafo4</p>											<!--info-->
+						</article>
+						<article>
+							<div class="image round">		
+								<img src="recursos/imagen2.jpg" alt="Pic 02" />
+							</div>
+							<header>	
+								<h3>TITULO5</h3>									<!--info-->
+							</header>
+							<p>parrafo5</p>											<!--info-->
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+
+					<h3>En contacto</h3>											<!--contacto-->
+
+					<form action="#" method="post">
+						<div class="field half">
+							<h4 id="email">Email</h4>
+							<input name="email" id="email" type="email" placeholder="Email"><!--INPUT-->
+						</div>	
+						<div class="field">
+							<h4 id="message">Mensaje</h4>
+							<textarea name="message" id="message" rows="6" placeholder="Message"></textarea> <!--texarea-->
+						</div>
+						<ul class="actions">
+							<li><input value="Send Message" class="button alt" type="submit"></li>	<!--SUMBIT-->
+						</ul>
+					</form>
+
+					<div class="copyright">
+						<a href="https://www.kaabcode.com/">Kaab_Code</a>				<!--fuentes o pie de pag-->
+					</div>
+
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="js/jquery.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+
+	</body>
+</html>
---
+++ css/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">  
+        <title>Login GasValid</title>                                       <!--Titulo a pestañaa-->
+        <link rel ="icon" type="icon/png" href="recursos/gasvalid_logo.png">           <!--Icono a pestaña-->
+        <link rel="stylesheet" type="text/css" href="css/login.css">        <!--estilo del login css-->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" type="text/css" href="libraries/bootstrap4/bootstrap.min.css"> <!--libreria Para el cel-->
+    </head>
+    <body>
+  <div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Icon -->
+    <div class="fadeIn first">
+      <img src="recursos/gasvalid_logo.png"id="icon" alt="User Icon" />                <!--Imagen de presentacion-->
+      <h1>Inicio de Sesion</h1>                                                 <!--Titulo-->
+    </div>
+    <!-- Creamos el Formulario de login-->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="user"> <!--input usuario-->
+      <input type="password" id="password" class="fadeIn third" name="login" placeholder="password"> <!--input password-->
+      <input type="submit" class="fadeIn fourth" value="Entrar">                              <!--Enviar info para entrar-->
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="inicio.php">HOME</a>        <!--Hacemos un vinculo para hacer el registro-->
+    </div>
+
+  </div>
+</div>                     
+    </body>
+</html>
