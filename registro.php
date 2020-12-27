1ddcfbecffa1d3ea1375d76e60f3583c1aa3830c
RobertoNeri <75956181+RobertoNeri@users.noreply.github.com>
2020-12-26 22:54:28 -0600

    actualizacion de valores
    modifique el archivo para hacer vinculo con los iconos y corregir error en sintaxis como un espacio entre codigos.

--- registro.php
+++ registro.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">  
         <title>Registro GasValid</title>                                       <!--Titulo a pestañaa-->
-        <link rel ="icon" type="icon/png" href="recursos/gasicon.png">           <!--Icono a pestaña-->
+        <link rel ="icon" type="icon/png" href="recursos/gasvalid_logo.jpeg">           <!--Icono a pestaña-->
         <link rel="stylesheet" type="text/css" href="css/registro.css">        <!--estilo del login css-->
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="libraries/bootstrap4/bootstrap.min.css"> <!--libreria Para el cel-->
@@ -13,10 +13,10 @@
   <div id="formContent">
     <!-- Icon -->
     <div class="fadeIn first">
-      <img src="recursos/gasicon.png"id="icon" alt="User Icon" />                <!--Imagen de presentacion-->
+      <img src="recursos/gasvalid_logo.jpeg" id="icon" alt="User Icon" />                <!--Imagen de presentacion-->
       <h1>Registro de Clientes</h1>                                                 <!--Titulo-->
     </div>
-    <!-- Creamos el Formulario de login-->
+    <!-- Creamos el Formulario de registro-->
     <form>
       <input type="text" id="login" class="fadeIn noestacion" name="login" placeholder="No. de estacion"> <!--input usuario-->
       <input type="password" id="password" class="fadeIn password" name="login" placeholder="password"> <!--input password-->
@@ -24,12 +24,10 @@
       <input type="text" id="referencia" class="fadeIn referencia" name="referencia" placeholder="referencia"><!--input refer-->
       <input type="submit" class="fadeIn registrar" value="Registrar">                     <!--  Enviar info para registrar-->
     </form>
-
     <!-- Remind Passowrd -->
     <div id="formFooter">
       <a class="underlineHover" href="repositorio.php">REPOSITORIO</a>        <!--Hacemos un vinculo para hacer el registro-->
     </div>
-
   </div>
 </div>                     
     </body>
